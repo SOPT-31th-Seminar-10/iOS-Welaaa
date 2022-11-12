a108cd6d6d01400c139f0c5b0601063180d8372a
seungchan <chanchu@hansung.ac.kr>
2022-11-12 22:29:00 +0900

    [#1] Color

---
+++ IN_SOPT_Welaaa/IN_SOPT_Welaaa/Util/Constant/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  IN_SOPT_Welaaa
+//
+//  Created by 김승찬 on 2022/11/12.
+//
+
+import UIKit
+
+public enum Color {
+    
+    public static let gray100 = UIColor(named: "gray100")!
+    public static let gray300 = UIColor(named: "gray300")!
+    public static let gray400 = UIColor(named: "gray400")!
+    public static let gray600 = UIColor(named: "gray600")!
+    public static let gray800 = UIColor(named: "gray800")!
+    public static let gray900 = UIColor(named: "gray900")!
+    public static let green = UIColor(named: "green")!
+}
